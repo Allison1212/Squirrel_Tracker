e2648bfe9c6d160d0081912646e6742c8a451a18
YanqiuX <xyq1996@gmail.com>
2020-10-15 11:04:37 -0700

    url update

--- sqtracker/urls.py
+++ sqtracker/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('sightings/', views.index, name='index'),
+    path('sightings/<unique_id>', views.index, name='index'),
+    path('sightings/add', views.index, name='index'),
+    path('sightings/stats', views.index, name='index'),
+    path('map/', views.index, name='index'),
 ]
