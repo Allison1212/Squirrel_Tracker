eaee2617f06926eee3a325043065341eaf5b74a2
YanqiuX <xyq1996@gmail.com>
2020-10-15 10:57:21 -0700

    models update  Please enter the commit message for your changes. Lines starting

--- sqtracker/models.py
+++ sqtracker/models.py
@@ -26,14 +26,16 @@ class Sightings(models.Model):
         (PM,_('PM')),
     ]
     shift = models.CharField(
-        max_length = 2,
+        max_length = 16,
         choices = SHIFT_CHOICES,
-        default = AM,
+        blank = True
     )
 
     #Date
     date = models.DateField(
         help_text = _('Date'),
+        null = True,
+        blank = True
     )
 
     #Age
@@ -49,11 +51,23 @@ class Sightings(models.Model):
         max_length = 20,
         choices = AGE_CHOICES,
         default = JUVENILE,
+        blank = True
         )
 
     # Primary Fur Color
+    BLACK = 'Black'
+    Gray = 'Gray'
+    CINNAMON = 'Cinnamon'
+    COLOR_CHOICE=(
+            (BLACK,_'Black'),
+            (GRAY,_'Gray'),
+            (CINNAMON,_'Cinnamon'),
+            (None,_''),
+            )
     primary_fur_color = models.CharField(
-        max_length = 20,
+            max_length=16,
+            choices = COLOR_CHOICE,
+            blank = True
     )
 
     #Location
@@ -67,57 +81,85 @@ class Sightings(models.Model):
     location = models.CharField(
         max_length = 20,
         choices = LOCATION_CHOICES,
-        default = GROUND_PLANE,
+        blank = True
         )
 
     #Specific Location
     specific_location = models.CharField(
         max_length = 50,
+        blank = True
     )
 
     # Running
-    running = models.BooleanField()
+    running = models.BooleanField(
+        blank = True
+    )
 
     # Chasing
-    chasing = models.BooleanField()
+    chasing = models.BooleanField(
+        blank = True
+    )
 
     # Climbing
-    climbing = models.BooleanField()
+    climbing = models.BooleanField(
+        blank = True
+    )
 
     # Eating
-    eating = models.BooleanField()
+    eating = models.BooleanField(
+        blank = True
+    )
 
     # Foraging
-    foraging = models.BooleanField()
+    foraging = models.BooleanField(
+        blank = True
+    )
 
     # Other Activities
     other_activities = models.CharField(
         max_length = 100,
+        blank = True
     )
 
     # Kuks
-    kuks = models.BooleanField()
+    kuks = models.BooleanField(
+        blank = True
+    )
 
     # Quaas
-    quaas = models.BooleanField()
+    quaas = models.BooleanField(
+        blank = True
+    )
 
     # Moans
-    moans = models.BooleanField()
+    moans = models.BooleanField(
+        blank = True
+    )
 
     # Tail flags
-    tail_flags = models.BooleanField()
+    tail_flags = models.BooleanField(
+        blank = True
+    )
 
     # Tail twitches
-    tail_twitches = models.BooleanField()
+    tail_twitches = models.BooleanField(
+        blank = True
+    )
 
     # Approaches
-    approaches = models.BooleanField()
+    approaches = models.BooleanField(
+        blank = True
+    )
 
     # Indifferent
-    indifferent = models.BooleanField()
+    indifferent = models.BooleanField(
+        blank = True
+    )
 
     # Runs from
-    runs_from = models.BooleanField()
+    runs_from = models.BooleanField(
+        blank = True
+    )
 
     def __str__(self):
         return self.unique_squirrel_id
